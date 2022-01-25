c020b457229799e8558c0e071ed38453dbb8fd5d
jay ber <jayber1@gmail.com>
2022-01-25 15:19:18 +0200

    to work at home

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
+
 from flask import Flask, render_template, redirect, request, url_for,Request
 
 app = Flask(__name__)
 
-@app.route('/')
+# https://www.alittlesparkofjoy.com/tarot-cards-list/
+
+@app.route('/', methods=['GET','POST'])
 def login():
+    if request.method == 'GET':
+        data = request.args
+        print(data)
+        return redirect(url_for('user_interface'))
     return render_template('index.html')
 
+@app.route('/user')
+def user_interface():
+    return render_template('user_interface.html')
 if '__main__' == __name__:
     app.run(debug=True)

aad87a74300b099beb42cede5f9d432d1ed91cb4
jay ber <jayber1@gmail.com>
2022-01-26 13:42:37 +0200

    working form

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 
 from flask import Flask, render_template, redirect, request, url_for,Request
-
+from flask_login import login_manager
 app = Flask(__name__)
 
 # https://www.alittlesparkofjoy.com/tarot-cards-list/
 
 @app.route('/', methods=['GET','POST'])
 def login():
-    if request.method == 'GET':
-        data = request.args
+    if request.method == 'POST':
+        data = request.form.get('email')
         print(data)
-        return redirect(url_for('user_interface'))
+        return redirect(url_for('user_interface',email=data))
     return render_template('index.html')
 
-@app.route('/user')
-def user_interface():
-    return render_template('user_interface.html')
+@app.route('/user/<string:email>')
+def user_interface(email):
+    return render_template('user_interface.html', username=email)
 if '__main__' == __name__:
     app.run(debug=True)

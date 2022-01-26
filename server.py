0a4b93d69e1818d3d0286496a8bafd80ce68540d
jay ber <jayber1@gmail.com>
2022-01-26 13:49:40 +0200

    with simple user auth

--- server.py
+++ server.py
@@ -5,16 +5,23 @@ app = Flask(__name__)
 
 # https://www.alittlesparkofjoy.com/tarot-cards-list/
 
+user = {
+    'email':'jayber1@gmail.com',
+    'password':'123',
+}
+
 @app.route('/', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        data = request.form.get('email')
-        print(data)
-        return redirect(url_for('user_interface',email=data))
+        email = request.form.get('email')
+        password = request.form.get('pass')
+        if email == user['email'] and user['password'] == password:
+            
+            return redirect(url_for('user_interface',email=email,password=password))
     return render_template('index.html')
 
-@app.route('/user/<string:email>')
-def user_interface(email):
-    return render_template('user_interface.html', username=email)
+@app.route('/user/<string:email>?<string:password>')
+def user_interface(email,password):
+    return render_template('user_interface.html', username=email, password=password)
 if '__main__' == __name__:
     app.run(debug=True)

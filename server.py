f868772ba892aee95ca5f4d751c54a6747b61928
jay ber <jayber1@gmail.com>
2022-02-08 16:20:51 +0200

    with little database

--- server.py
+++ server.py
@@ -1,15 +1,27 @@
 
+from unittest import result
 from flask import Flask, render_template, redirect, request, url_for,Request
 from flask_login import login_manager
 from flask_bootstrap import Bootstrap
 import random
 from os import listdir
 from os.path import isfile, join
+import sqlite3
+from sqlalchemy import create_engine, text
 import json
+from flask_sqlalchemy import SQLAlchemy
 
+# TODO:
+# -- build a DB scheme on paper
+# -- add a user table DB
+# -- add USER LOGIN with DB and login manager
+# -- add a user panel HTML page to view diary
 
+#  ------------------------ app configuration ------------------------
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # https://www.alittlesparkofjoy.com/tarot-cards-list/
 # https://www.biddytarot.com/tarot-card-meanings/minor-arcana/suit-of-pentacles/king-of-pentacles/
 # https://labyrinthos.co/blogs/tarot-card-meanings-list
@@ -18,6 +30,17 @@ user = {
     'password':'123',
     'allowed': True,
 }
+
+
+
+# ----------------------- datebase handling -------------------------
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    age = db.Column(db.INTEGER())
+
+db.create_all()
 
 
 @app.route('/', methods=['GET','POST'])
